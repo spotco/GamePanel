df807688c7a355a0b2964e5292540adda5419977
spotco <mootothemax@gmail.com>
2013-02-11 01:38:41 -0800

    working

---
+++ GamePanel.java
@@ -0,0 +1,93 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class GamePanel extends DrawingPanel {
+	
+	protected static int KEY_UP = KeyEvent.VK_UP;
+	protected static int KEY_DOWN = KeyEvent.VK_DOWN;
+	protected static int KEY_LEFT = KeyEvent.VK_LEFT;
+	protected static int KEY_RIGHT = KeyEvent.VK_RIGHT;
+	
+	private Set<Integer> _keys_down;
+	private boolean _mouse_down;
+	private int _mouseX,_mouseY;
+	protected Graphics _g;
+	
+	public GamePanel(int width, int height) {
+		super(width, height);
+		_keys_down = new HashSet<Integer>();
+		_g = this.getGraphics();
+	}
+	
+	//ignore all of these methods
+	@Override public void mouseMoved(MouseEvent arg0){ _mouseX = arg0.getX(); _mouseY = arg0.getY();}
+	@Override public void mousePressed(MouseEvent arg0) { _mouse_down = true;}
+	@Override public void mouseReleased(MouseEvent arg0) {_mouse_down = false;}
+	@Override public void keyPressed(KeyEvent arg0) {_keys_down.add(arg0.getKeyCode());}
+	@Override public void keyReleased(KeyEvent arg0) {_keys_down.remove(arg0.getKeyCode());}
+	
+	//use these methods
+	/**
+	 * Clears screen
+	 */
+	public void clear() {
+		_g.setColor(Color.WHITE);
+		_g.fillRect(0, 0, this.getSize().width, this.getSize().height);
+	}
+	
+	/**
+	 * @return width of screen
+	 */
+	public int get_width() {
+		return this.getSize().width;
+	}
+	
+	/**
+	 * @return height of the screen
+	 */
+	public int get_height() {
+		return this.getSize().height;
+	}
+	
+	/**
+	 * @param key - keycode of target key
+	 * @return if target key is currently pressed
+	 */
+	public boolean is_key_down(int key) {
+		return _keys_down.contains(key);
+	}
+	
+	/**
+	 * @return if mouse is down
+	 */
+	public boolean is_mouse_down() {
+		return _mouse_down;
+	}
+	
+	/**
+	 * @return current x position of mouse
+	 */
+	public int get_mouse_x() {
+		return _mouseX;
+	}
+	
+	/**
+	 * @return current y position of mouse
+	 */
+	public int get_mouse_y() {
+		return _mouseY;
+	}
+	
+	//override this method
+	/**
+	 * Continuously called, override with game logic
+	 */
+	@Override
+	public void update(){
+	}
+}
---
+++ SimpleGameMain.java
@@ -0,0 +1,55 @@
+import java.awt.Color;
+
+
+public class SimpleGameMain extends GamePanel {
+	
+	public static void main(String[] args) {
+		new SimpleGameMain(300,300);
+	}
+	
+	public int _player_x,_player_y;
+	public int _food_x,_food_y;
+
+	public SimpleGameMain(int width, int height) {
+		super(width, height);
+		move_food();
+	}
+	
+	public void move_food(){
+		_food_x = (int) (Math.random()*get_width());
+		_food_y = (int) (Math.random()*get_height());
+	}
+	
+	public double get_food_dist() {
+		return Math.sqrt(Math.pow(_player_x-_food_x,2)+Math.pow(_player_y-_food_y,2));
+	}
+	
+	@Override
+	public void update() {
+		this.clear();
+		
+		if (this.is_key_down(KEY_UP)) {
+			_player_y-=5;
+		}
+		if (is_key_down(KEY_DOWN)) {
+			_player_y+=5;
+		} 
+		if (is_key_down(KEY_LEFT)) {
+			_player_x-=5;
+		}
+		if (is_key_down(KEY_RIGHT)) {
+			_player_x+=5;
+		}
+		
+		if (get_food_dist()<15) {
+			move_food();
+		}
+		
+		_g.setColor(Color.RED);
+		_g.fillOval(_player_x, _player_y, 20, 20);
+		
+		_g.setColor(Color.GREEN);
+		_g.fillOval(_food_x, _food_y, 10, 10);
+	}
+
+}
